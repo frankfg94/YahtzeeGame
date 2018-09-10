5898ef1e6fa1f79c31f040cbab3ed5cb0a277bb9
franc <francoisgillioen@live.fr>
2018-09-10 17:03:47 -0400

    2 bug fixes

---
+++ YzeeCours/src/yzeecours/core/Main.java
@@ -0,0 +1,12 @@
+package core;
+
+public class Main 
+{	
+	public static void main(String[] args)
+	{
+		// Welcome to the Yahtzee GITHUB project
+		YahtzeeGame game = new YahtzeeGame();
+		game.ShowMenu();
+	}
+	 
+}
